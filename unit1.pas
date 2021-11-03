f9732c2921d85462521f6a3c9dbda7e0b3e6ea3f
Toru-Hongo <hongo.toru@gmail.com>
2021-11-03 20:41:32 +0900

    First commit Lazarus

---
+++ unit1.pas
@@ -0,0 +1,47 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+  Label1.Caption:='Hello, Lazarus 2.0.12';
+  Button1.Caption:='OK';
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  ShowMessage('終了しますか');
+  Close();
+end;
+
+end.
+
